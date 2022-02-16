e1e766bf678fb6c7efdd4e85c27d752bbfc32fc9
Chrissie Zimmer <czimmer12@aol.com>
2022-02-16 17:20:44 -0500

    knapSack

---
+++ c++basics/cppHomeworkBasic/assignment.cpp
@@ -0,0 +1,6 @@
+cout << "Enter two numbers: ";   
+cin >> a >> b;   
+a = a - 5.0;   
+b = a * b;   
+cout << "a = " << a << endl;   
+cout << "b = " << b << endl; 
---
+++ c++notes/knapSack.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int max(int a, int b){
+    return(a > b) ?  a : b;}
+}
+
+int main()
+{
+   int val[] = {60, 100, 120};
+   int wt[] = {10, 20, 30};
+   int W = 50;
+   int n = sizeof(val) / sizeof(val[0]);
+   cout << knapSack(W, wt, val, n);
+   return 0;
+}
+int knapSack(int W, int wt[], int val[], int n){
+    if(n==0 || W == 0)
+        return 0;
+        
+    if(wt[n-1]> W){
+        return knapSack(W, wt, val, n-1);
+    }
+}
